31e8ff6b8f4a65aa9bdee92ea2fb7b75d5d6315b
unknown <aadhya.bhardwaj31@gmail.com>
2026-06-18 10:03:09 +0530

    experiments

--- employee-crud-api/src/main/java/com/example/employeeapi/model/EmployeeApiApplication.java
+++ employee-crud-api/src/main/java/com/example/employeeapi/model/EmployeeApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public clacss EmployeeApiApplication {
+public class EmployeeApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApiApplication.class, args);
     }
